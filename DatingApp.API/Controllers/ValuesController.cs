9457f53b6e6688254e19b6baee924ac0ff712483
Rocio <anarocio26@gmail.com>
2019-12-23 12:14:39 -0600

    First Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    // Get API Values
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        // IActionResult allows us to get HTTP responeses
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // Get with ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // Post API Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+        // Put API Values
+        [HttpPut]
+
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+        // Delete API Values
+        [HttpDelete]
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
