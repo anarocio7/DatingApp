ac82ea673e77b0555095e949354974fe147163c2
Rocio <anarocio26@gmail.com>
2020-01-27 17:49:30 -0600

    Added edit component to SPA and updateDTO to API

--- DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -5,21 +5,23 @@ import { User } from '../_models/user.interface';
 import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
 
 // Basically, a Resolver acts like middleware,
 // which can be executed before a component is loaded
 
 @Injectable()
-export class MemberDetailResolver implements Resolve<User>{
+export class MemberEditResolver implements Resolve<User>{
   constructor(private alertifyService: AlertifyService,
+              private auth: AuthService,
               private userService: UserService,
               private router: Router) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<User> {
     // tslint:disable-next-line:no-string-literal
-    return this.userService.getUser(route.params['id']).pipe(
+    return this.userService.getUser(this.auth.decodedToken.nameid).pipe(
       catchError(error => {
-        this.alertifyService.error('There is no retrieving data');
+        this.alertifyService.error('Problem retrieving data');
         this.router.navigate(['/members']);
         return of(null);
       })
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,9 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -45,7 +48,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MemberCardComponent,
     ListsComponent,
     MessagesComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -66,6 +70,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
               ErrorInterceptorProvider,
               MemberDetailResolver,
               MemberListResolver,
+              MemberEditResolver,
+              PreventUnsavedChanges,
               { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}],
   bootstrap: [AppComponent]
 })
